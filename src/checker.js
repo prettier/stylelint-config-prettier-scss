a19faabb767219e7b4cf74e41dedd2a051501fdf
stephen.liu <stephen.liu@fipo.cn>
2023-01-12 13:19:27 +0800

    fix: fix TypeError: stylelint.createLinter(...).getConfigForFile is not a function

--- src/checker.js
+++ src/checker.js
@@ -7,39 +7,55 @@ const { resolve } = require('path');
 
 const { hasOwnProperty } = Object.prototype;
 
-function check(path) {
+function tryRequire(mod) {
+	try {
+		return require(mod);
+	} catch (error) {
+		return null;
+	}
+}
+
+const getConfigForFile = tryRequire('stylelint/lib/getConfigForFile');
+
+async function getConfig(linter, resolvedPath) {
+	if (linter.getConfigForFile) {
+		return await linter.getConfigForFile(resolvedPath);
+	}
+
+	if (getConfigForFile) {
+		return await getConfigForFile(linter, resolvedPath);
+	}
+
+	throw new Error(`Could not get config for file: ${resolvedPath}`);
+}
+
+async function check(path) {
 	const resolvedPath = resolve(process.cwd(), path || '');
+	const linter = stylelint.createLinter();
+	const config = await getConfig(linter, resolvedPath);
 
-	return stylelint
-		.createLinter()
-		.getConfigForFile(resolvedPath)
-		.then((config) => {
-			const prettierRules = {
-				...cssConfig.rules,
-				...scssConfig.rules,
-			};
-			const configRules = config.config.rules;
-			const conflictingRules = [];
+	const prettierRules = {
+		...cssConfig.rules,
+		...scssConfig.rules,
+	};
+	const configRules = config.config.rules;
+	const conflictingRules = [];
 
-			function isEmpty(value) {
-				return (
-					value === null ||
-					(Array.isArray(value) && value.length === 0)
-				);
-			}
+	function isEmpty(value) {
+		return value === null || (Array.isArray(value) && value.length === 0);
+	}
 
-			Object.keys(prettierRules).forEach((rule) => {
-				if (
-					hasOwnProperty.call(configRules, rule) &&
-					!isEmpty(configRules[rule]) &&
-					configRules[rule][0] !== prettierRules[rule]
-				) {
-					conflictingRules.push(rule);
-				}
-			});
+	Object.keys(prettierRules).forEach((rule) => {
+		if (
+			hasOwnProperty.call(configRules, rule) &&
+			!isEmpty(configRules[rule]) &&
+			configRules[rule][0] !== prettierRules[rule]
+		) {
+			conflictingRules.push(rule);
+		}
+	});
 
-			return conflictingRules.length ? conflictingRules : null;
-		});
+	return conflictingRules.length ? conflictingRules : null;
 }
 
 exports.check = check;
